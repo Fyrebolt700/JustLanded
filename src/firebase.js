3b4edbc4287d9aed1289dcf495a345a713aa89cd
Suhaani Singh <suhaanis47@gmail.com>
2026-03-07 15:36:16 -0500

    add firebase and auth0 configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "hack-canada-2026.firebaseapp.com",
-  projectId: "hack-canada-2026",
-  storageBucket: "hack-canada-2026.firebasestorage.app",
-  messagingSenderId: "613364990016",
-  appId: "1:613364990016:web:9a419cbc12b220feaa1fa5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { StrictMode } from 'react'
-import './index.css'
-import App from './App.jsx'
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { auth0Config } from "./auth0-config";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Auth0Provider {...auth0Config}>
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
