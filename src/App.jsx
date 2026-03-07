eaee9679110c8e8896e4686510de73c0b17ed34c
Suhaani Singh <suhaanis47@gmail.com>
2026-03-07 16:51:21 -0500

    dashboard and layout fully working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
 import LandingPage from "./pages/LandingPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -8,23 +7,17 @@ import ChatbotPage from "./pages/ChatbotPage";
 import MapPage from "./pages/MapPage";
 import OnboardingPage from "./pages/OnboardingPage";
 
-function ProtectedRoute({ children }) {
-  const { isAuthenticated, isLoading } = useAuth0();
-  if (isLoading) return <div>Loading...</div>;
-  return isAuthenticated ? children : <Navigate to="/" />;
-}
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/onboarding" element={<ProtectedRoute><OnboardingPage /></ProtectedRoute>} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
         <Route element={<AppLayout />}>
-          <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-          <Route path="/checklist" element={<ProtectedRoute><ChecklistPage /></ProtectedRoute>} />
-          <Route path="/chatbot" element={<ProtectedRoute><ChatbotPage /></ProtectedRoute>} />
-          <Route path="/map" element={<ProtectedRoute><MapPage /></ProtectedRoute>} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/checklist" element={<ChecklistPage />} />
+          <Route path="/chatbot" element={<ChatbotPage />} />
+          <Route path="/map" element={<MapPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
